b5f171c29734cfde859cd42f370b3d1ab2b56573
kvellur <karthik_vellur@intuit.com>
2020-03-26 12:28:59 +0530

    feat: Insertion sort

--- src/main/java/InsertionSort.java
+++
@@ -1,7 +0,0 @@
-public class InsertionSort {
-
-    public static void main(String[] args){
-
-        int[] array = {5,2,4,6,1,3};
-    }
-}
---
+++ src/main/java/insertionSort/InsertionSort.java
@@ -0,0 +1,24 @@
+package insertionSort;
+
+public class InsertionSort {
+
+    public static void main(String[] args){
+
+        int[] array = {31,41,59,26,41,58};
+        int key;
+
+        for(int j=1; j<=array.length-1; j++){
+            key = array[j];
+            int i = j+1;
+            while(i>=0 && array[i] > key){
+                array[i+1] = array[i];
+                i--;
+            }
+            array[i+1] = key;
+        }
+
+        for(int i=0;i<array.length;i++)
+            System.out.println(array[i]);
+
+    }
+}
