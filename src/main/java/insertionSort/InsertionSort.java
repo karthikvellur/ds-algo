5b293829311ca6d110665369951fc2a931cc98c5
kvellur <karthik_vellur@intuit.com>
2020-03-26 12:35:25 +0530

    feat: Insertion sort - descending order

--- src/main/java/insertionSort/InsertionSort.java
+++ src/main/java/insertionSort/InsertionSort.java
@@ -4,12 +4,21 @@ public class InsertionSort {
 
     public static void main(String[] args){
 
+        ascendingOrderSort();
+        descendingOrderSort();
+
+    }
+
+    static void ascendingOrderSort(){
+
+        System.out.println("Ascending order");
+
         int[] array = {31,41,59,26,41,58};
         int key;
 
         for(int j=1; j<=array.length-1; j++){
             key = array[j];
-            int i = j+1;
+            int i = j-1;
             while(i>=0 && array[i] > key){
                 array[i+1] = array[i];
                 i--;
@@ -19,6 +28,26 @@ public class InsertionSort {
 
         for(int i=0;i<array.length;i++)
             System.out.println(array[i]);
+    }
 
+    static void descendingOrderSort(){
+
+        System.out.println("Descending order");
+
+        int[] array = {31,41,59,26,41,58};
+        int key;
+
+        for(int j=1; j<=array.length-1; j++){
+            key = array[j];
+            int i = j-1;
+            while(i>=0 && array[i] < key){
+                array[i+1] = array[i];
+                i--;
+            }
+            array[i+1] = key;
+        }
+
+        for(int i=0;i<array.length;i++)
+            System.out.println(array[i]);
     }
 }
