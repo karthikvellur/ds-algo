bc5553ea34c7865c4e20b875381e23a2a925faa5
kvellur <karthik_vellur@intuit.com>
2020-03-30 15:07:35 +0530

    chore: updated references

--- src/main/java/ds/bst.java
+++ src/main/java/ds/bst.java
@@ -1,6 +1,10 @@
 package ds;
 
 
+// Reference: https://www.techiedelight.com/binary-search-tree-bst-interview-questions/
+// Worst case time complexity for all operations (search, insert, delete) on BST is O(h) where h is the height of the tree as in worst case we will have to traverse through all the levels of tree
+// https://www.geeksforgeeks.org/complexity-different-opWorst%20case%20time%20complexity%20for%20all%20operations%20(search,%20insert,%20delete)%20on%20BST%20is%20O(h)%20where%20h%20is%20the%20height%20of%20the%20tree%20as%20in%20worst%20case%20we%20will%20have%20to%20traverse%20through%20all%20the%20levels%20of%20treerations-binary-tree-binary-search-tree-avl-tree/
+
 class Node{
     int data;
 
