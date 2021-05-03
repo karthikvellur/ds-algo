67933eb79eb23af09f052f5fbb663735f46586cf
Karthik Vellur <karthik_vellur@intuit.com>
2021-05-03 07:29:24 +0530

    feat: js - merged intervals algo

---
+++ src/main/js/ds/array.js
@@ -0,0 +1,32 @@
+// https://medium.com/swlh/how-to-merge-intervals-step-by-step-cb2c1edf1cd0
+function mergeIntervals(array) {
+    if(array.length < 2)
+        return array;
+
+    array.sort((a, b) => a[0] - b[0]);
+
+    let startTime = array[0][0];
+    let endTime = array[0][1];
+    let curStart;
+    let curEnd;
+    const intervals = [];
+
+    for(let i=1; i<array.length; i++){
+        curStart = array[i][0];
+        curEnd = array[i][1];
+
+        if(curStart <= endTime){
+            // Main condition is this "if" statement where is we identify if there exists any interval whose start is less than or equal to the defined "endTime",
+            // that means it is a merged interval
+            endTime = Math.max(endTime, curEnd);
+        }else{
+            intervals.push([startTime, endTime]);
+            startTime = curStart;
+            endTime = curEnd;
+        }
+    }
+    intervals.push([startTime, endTime]);
+    return intervals;
+}
+
+console.log(mergeIntervals([[0,5], [1,4], [2,5], [7,9]]));
